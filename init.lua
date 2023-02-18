3c54bc196adc56c8ed7c8583d92d6dc621baeaa3
Raju Ghorai <amirajubolchi001@gmail.com>
2023-02-19 02:19:34 +0530

    SmoothScroll plugin added

--- init.lua
+++ init.lua
@@ -103,44 +103,7 @@ require('packer').startup(function(use)
     },
     config = function()
       require('vacuumline').setup({
-        theme = require('vacuumline.theme.tokoynight_storm'),
-        mode_colors = {
-          n = '#81A1C1',
-          i = '#8FBCBB',
-          v = '#BF616A',
-          [''] = '#D08770',
-          V = '#EBCB8B',
-          c = '#A3BE8C',
-          no = '#B48EAD',
-          s = '#88C0D0',
-          S = '#81A1C1',
-          ic = '#D8DEE9',
-          R = '#E5E9F0',
-          Rv = '#EBCB8B',
-          cv = '#D08770',
-          ce = '#D08770',
-          r = '#A3BE8C',
-          rm = '#A3BE8C',
-          ['r?'] = '#A3BE8C',
-          ['!'] = '#BF616A',
-          t = '#EBCB8B',
-        },
-        sections = {
-          lualine_a = {},
-          lualine_b = {},
-          lualine_c = {
-            {
-              'filename',
-              file_status = true,
-              path = 1,
-            },
-          },
-          lualine_x = {},
-          lualine_y = {},
-          lualine_z = {},
-        },
-        section_separator = '',
-        component_separators = '',
+        theme = require('vacuumline.theme.tokoynight_storm')
       })
     end,
   }
@@ -207,6 +170,18 @@ require('packer').startup(function(use)
         ignore_filetypes = { "NvimTree", "TelescopePrompt" }
       })
     end
+  })
+
+  use({
+    "gen740/SmoothCursor.nvim",
+    config = function() require("smoothcursor").setup({
+      cursor = "",
+        fancy = {
+            enable = true,
+            head = {cursor = "", texthl = "SmoothCursor", linehl = nil}
+        }
+    })
+  end
   })
 
   -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
@@ -521,169 +496,7 @@ local servers = {
   jsonls = {
     settings = {
       json = {
-        schemas = {
-          {
-            description = "TypeScript compiler configuration file",
-            fileMatch = {
-              "tsconfig.json",
-              "tsconfig.*.json",
-            },
-            url = "https://json.schemastore.org/tsconfig.json",
-          },
-          {
-            description = "Lerna config",
-            fileMatch = { "lerna.json" },
-            url = "https://json.schemastore.org/lerna.json",
-          },
-          {
-            description = "Babel configuration",
-            fileMatch = {
-              ".babelrc.json",
-              ".babelrc",
-              "babel.config.json",
-            },
-            url = "https://json.schemastore.org/babelrc.json",
-          },
-          {
-            description = "ESLint config",
-            fileMatch = {
-              ".eslintrc.json",
-              ".eslintrc",
-            },
-            url = "https://json.schemastore.org/eslintrc.json",
-          },
-          {
-            description = "Bucklescript config",
-            fileMatch = { "bsconfig.json" },
-            url = "https://raw.githubusercontent.com/rescript-lang/rescript-compiler/8.2.0/docs/docson/build-schema.json",
-          },
-          {
-            description = "Prettier config",
-            fileMatch = {
-              ".prettierrc",
-              ".prettierrc.json",
-              "prettier.config.json",
-            },
-            url = "https://json.schemastore.org/prettierrc",
-          },
-          {
-            description = "Vercel Now config",
-            fileMatch = { "now.json" },
-            url = "https://json.schemastore.org/now",
-          },
-          {
-            description = "Stylelint config",
-            fileMatch = {
-              ".stylelintrc",
-              ".stylelintrc.json",
-              "stylelint.config.json",
-            },
-            url = "https://json.schemastore.org/stylelintrc",
-          },
-          {
-            description = "A JSON schema for the ASP.NET LaunchSettings.json files",
-            fileMatch = { "launchsettings.json" },
-            url = "https://json.schemastore.org/launchsettings.json",
-          },
-          {
-            description = "Schema for CMake Presets",
-            fileMatch = {
-              "CMakePresets.json",
-              "CMakeUserPresets.json",
-            },
-            url = "https://raw.githubusercontent.com/Kitware/CMake/master/Help/manual/presets/schema.json",
-          },
-          {
-            description = "Configuration file as an alternative for configuring your repository in the settings page.",
-            fileMatch = {
-              ".codeclimate.json",
-            },
-            url = "https://json.schemastore.org/codeclimate.json",
-          },
-          {
-            description = "LLVM compilation database",
-            fileMatch = {
-              "compile_commands.json",
-            },
-            url = "https://json.schemastore.org/compile-commands.json",
-          },
-          {
-            description = "Config file for Command Task Runner",
-            fileMatch = {
-              "commands.json",
-            },
-            url = "https://json.schemastore.org/commands.json",
-          },
-          {
-            description = "AWS CloudFormation provides a common language for you to describe and provision all the infrastructure resources in your cloud environment.",
-            fileMatch = {
-              "*.cf.json",
-              "cloudformation.json",
-            },
-            url = "https://raw.githubusercontent.com/awslabs/goformation/v5.2.9/schema/cloudformation.schema.json",
-          },
-          {
-            description = "The AWS Serverless Application Model (AWS SAM, previously known as Project Flourish) extends AWS CloudFormation to provide a simplified way of defining the Amazon API Gateway APIs, AWS Lambda functions, and Amazon DynamoDB tables needed by your serverless application.",
-            fileMatch = {
-              "serverless.template",
-              "*.sam.json",
-              "sam.json",
-            },
-            url = "https://raw.githubusercontent.com/awslabs/goformation/v5.2.9/schema/sam.schema.json",
-          },
-          {
-            description = "Json schema for properties json file for a GitHub Workflow template",
-            fileMatch = {
-              ".github/workflow-templates/**.properties.json",
-            },
-            url = "https://json.schemastore.org/github-workflow-template-properties.json",
-          },
-          {
-            description = "golangci-lint configuration file",
-            fileMatch = {
-              ".golangci.toml",
-              ".golangci.json",
-            },
-            url = "https://json.schemastore.org/golangci-lint.json",
-          },
-          {
-            description = "JSON schema for the JSON Feed format",
-            fileMatch = {
-              "feed.json",
-            },
-            url = "https://json.schemastore.org/feed.json",
-            versions = {
-              ["1"] = "https://json.schemastore.org/feed-1.json",
-              ["1.1"] = "https://json.schemastore.og/feed.json",
-            },
-          },
-          {
-            description = "Packer template JSON configuration",
-            fileMatch = {
-              "packer.json",
-            },
-            url = "https://json.schemastore.org/packer.json",
-          },
-          {
-            description = "NPM configuration file",
-            fileMatch = {
-              "package.json",
-            },
-            url = "https://json.schemastore.org/package.json",
-          },
-          {
-            description = "JSON schema for Visual Studio component configuration files",
-            fileMatch = {
-              "*.vsconfig",
-            },
-            url = "https://json.schemastore.org/vsconfig.json",
-          },
-          {
-            description = "Resume json",
-            fileMatch = { "resume.json" },
-            url = "https://raw.githubusercontent.com/jsonresume/resume-schema/v1.0.0/schema.json",
-          },
-        },
+        schemas = {},
       },
     },
     setup = {

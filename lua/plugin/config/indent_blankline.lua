890bb445db32d38418bdc3777781c38b0e44f0da
Raju Ghorai <amirajubolchi001@gmail.com>
2023-08-19 12:26:22 +0530

    ➕Added: indent_blankline config

---
+++ lua/plugin/config/indent_blankline.lua
@@ -0,0 +1,61 @@
+local M = {}
+
+function M.setup()
+  vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
+  vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
+  vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
+  vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
+  vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
+  vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+  vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
+  vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
+  vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
+  vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
+  vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
+  vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+  require('indent_blankline').setup {
+    char = '┊',
+    buftype_exclude = { "terminal" },
+    show_trailing_blankline_indent = false,
+    show_current_context = true,
+    -- show_current_context_start = true,
+    filetype_exclude = {
+      "help",
+      "terminal",
+      "alpha",
+      "nvim-tree",
+      "neogit",
+      "diffview",
+    },
+    context_patterns = {
+      "class",
+      "function",
+      "method",
+      "^if",
+      "^while",
+      "^for",
+      "^object",
+      "^table",
+      "^type",
+      "^import",
+      "block",
+      "arguments"
+    },
+    use_treesitter = true,
+    space_char_blankline = " ",
+    char_highlight_list = {
+      "IndentBlanklineIndent1",
+      "IndentBlanklineIndent2",
+      "IndentBlanklineIndent3",
+      "IndentBlanklineIndent4",
+      "IndentBlanklineIndent5",
+      "IndentBlanklineIndent6",
+      "IndentBlanklineIndent7",
+      "IndentBlanklineIndent8",
+      "IndentBlanklineIndent9",
+      "IndentBlanklineIndent10",
+    },
+  }
+end
+
+return M

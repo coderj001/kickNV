4bbe8620e59f0f0103c7a81a34dacae1fd3991c5
Raju Ghorai <rajughorai41410@gmail.com>
2023-10-30 21:14:33 +0530

     Added: neogit and gitsigns

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -86,6 +86,27 @@ return {
       end
     }
   },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("plugins.extras.gitsigns").setup()
+    end
+  },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "sindrets/diffview.nvim",
+        lazy = true,
+        enabled = true,
+        event = "BufRead",
+      }
+    },
+    config = function()
+      require("plugins.extras.neogit").setup()
+    end
+  },
   { "jiangmiao/auto-pairs", event = "BufEnter" },
   { "tpope/vim-surround",   event = "BufEnter" },
   { "tpope/vim-repeat",     event = "BufEnter" },

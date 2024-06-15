9ec93f22be7eb54b08794fba0699dba03b61989b
Raju Ghorai <rajughorai41410@gmail.com>
2024-06-15 11:37:37 +0530

    update oil.nivm config and add gx plugin

--- lua/plugins/extras/explorer/oil.lua
+++ lua/plugins/extras/explorer/oil.lua
@@ -1,22 +1,31 @@
 return {
-    "stevearc/oil.nvim",
-    event = "VimEnter",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    keys = {
-      { "<leader>e", mode = { "n" }, "<CMD>Oil<CR>",   desc = "Oil Explorer Current" },
-      { "<leader>E", mode = { "n" }, "<CMD>Oil .<CR>", desc = "Oil Explorer Root" },
+  "stevearc/oil.nvim",
+  event = "VimEnter",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  keys = {
+    { "<leader>e", mode = { "n" }, "<CMD>Oil<CR>",   desc = "Oil Explorer Current" },
+    { "<leader>E", mode = { "n" }, "<CMD>Oil .<CR>", desc = "Oil Explorer Root" },
+  },
+  opts = {
+    default_file_explorer = true,
+    delete_to_trash = true,
+    skip_confirm_for_simple_edits = true,
+    view_options = {
+      show_hidden = true,
+      natural_order = true,
+      is_always_hidden = function(name, _)
+        return  name == ".." or name == ".git" or name == ".DS_Store" or name:match("^%.")
+      end
     },
-    opts = {
-      default_file_explorer = true,
-      columns = {
-        "icon",
-        "permissions",
-        "size",
-        "mtime",
-      },
-      buf_options = {
-        buflisted = false,
-        bufhidden = "hide",
-      },
+    columns = {
+      "icon",
+      "permissions",
+      "size",
+      "mtime",
     },
-  }
+    buf_options = {
+      buflisted = false,
+      bufhidden = "hide",
+    },
+  },
+}

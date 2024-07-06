5f66d7c0ac7ce3ecfb6af88dcb3eb6d4afa1b455
Raju Ghorai <rajughorai41410@gmail.com>
2024-07-06 15:57:09 +0530

    updated keymaps for config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,12 +19,12 @@ map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", expr_opts)
 map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", expr_opts)
 
 -- Resize window using <ctrl> arrow keys
-map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
-map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
-map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+map("n", "<C-Down>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+map("n", "<C-Up>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+map("n", "<C-Right>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+map("n", "<C-Left>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
 
--- Move Lines
+-- Move text up and down
 map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
 map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
 map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
@@ -34,6 +34,9 @@ map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 
 -- Clear search with <leader><Enter>
 map({ "i", "n" }, "<leader><Enter>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
+
+-- Save file
+map("n", "<leader>w", "<cmd>w<cr>", opts)
 
 function load_extra_options()
   local function bind_extra_cmd(options)

eb4304e6f73f9770ea1040ca3533df825dc8b1d3
Raju Ghorai <rajughorai41410@gmail.com>
2025-01-18 17:28:46 +0530

    update: keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -21,8 +21,8 @@ map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", expr_opts)
 -- Resize window using <ctrl> arrow keys
 map("n", "<C-Down>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
 map("n", "<C-Up>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-map("n", "<C-Right>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
-map("n", "<C-Left>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
 
 -- Move text up and down
 map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
